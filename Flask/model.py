88c1973b4337fe059664764067c984bd0400a467
Namrata <124352728+Namsrkive@users.noreply.github.com>
2025-03-17 16:54:23 +0530

    Update model.py

--- Flask/model.py
+++ Flask/model.py
@@ -19,43 +19,24 @@ class Plant_Disease_Model(nn.Module):
         return out
 
 
-# Define image transformations
 transform = transforms.Compose(
     [transforms.Resize(size=128),
      transforms.ToTensor()])
 
-# Define class labels
-num_classes = [
-    'Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
-    'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy',
-    'Corn_(maize)___Cercospora_leaf_spot_Gray_leaf_spot', 'Corn_(maize)___Common_rust_',
-    'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot',
-    'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy',
-    'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy',
-    'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight',
-    'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy',
-    'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy',
-    'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold',
-    'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites_Two-spotted_spider_mite',
-    'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
-    'Tomato___healthy'
-]
+num_classes = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy',
+               'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
 
-# Load the model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = Plant_Disease_Model().to(device)
-model.load_state_dict(torch.load('./Models/crop_disease_detection.pth', map_location=device))
+
+model = Plant_Disease_Model()
+model.load_state_dict(torch.load(
+    './Models/plantDisease-resnet34.pth', map_location=torch.device('cpu')))
 model.eval()
 
 
 def predict_image(img):
-    try:
-        img_pil = Image.open(io.BytesIO(img))
-        tensor = transform(img_pil).to(device)
-        xb = tensor.unsqueeze(0)
-        yb = model(xb)
-        _, preds = torch.max(yb, dim=1)
-        return num_classes[preds[0].item()]
-    except Exception as e:
-        print(f"Error predicting image: {e}")
-        return None
+    img_pil = Image.open(io.BytesIO(img))
+    tensor = transform(img_pil)
+    xb = tensor.unsqueeze(0)
+    yb = model(xb)
+    _, preds = torch.max(yb, dim=1)
+    return num_classes[preds[0].item()]
